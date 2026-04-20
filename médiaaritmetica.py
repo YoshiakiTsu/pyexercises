bd633e94a15b2b8ef6b9260bd903c1348c1246dd
YoshiakiTsu <154010380+YoshiakiTsu@users.noreply.github.com>
2026-04-19 23:06:21 -0300

    Add files via upload

---
+++ médiaaritmetica.py
@@ -0,0 +1,6 @@
+print ("insira 3 números inteiros")
+input1 = int(input("primeiro número: "))
+input2 = int(input("segundo número: "))
+input3 = int(input("terceiro número: "))
+média = (input1+input2+input3)/3
+print ("a média dos números é: ", média)
